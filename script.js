6b8a54a5911183124676141e73ebb5d80741257a
oybekweb9646 <oybekshodmonov9646@gmail.com>
2021-07-19 20:12:01 +0500

    first commit

--- script.js
+++ script.js
@@ -206,3 +206,17 @@ $('#owl-demo').owlCarousel({
     }
 })
 
+$(".open-btn").click(function () {
+    $("#search-wrap").addClass('panelactive');//#search-wrapへpanelactiveクラスを付与
+    $('#search-text').focus();//テキスト入力のinputにフォーカス
+});
+
+//閉じるボタンを押した時には
+$(".close-btn").click(function () {
+    $("#search-wrap").removeClass('panelactive');//#search-wrapからpanelactiveクラスを除去
+});
+
+
+
+
+Resources
